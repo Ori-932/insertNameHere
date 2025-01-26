00d2a4b3971e4d385b16f35e4bda9448f16cc791
Ori-932 <oriiii932@gmail.com>
2025-01-25 18:21:14 -0800

    try

--- global.js
+++ global.js
@@ -30,16 +30,21 @@ document.body.prepend(nav);
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
+
+    if (!url.startsWith('/')) {
+        url = '/' + url;
+    }
+
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
 
     a.classList.toggle('current', a.host === location.host && a.pathname === location.pathname);
 
-
     if (a.host !== location.host) {
         a.target = '_blank';
     }
+
     nav.append(a);
 }
 
